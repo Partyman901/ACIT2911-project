636b753070775080ab1e327a4ea55e70896f8b9d
shaniah <shaniah.nizzar@hotmail.com>
2021-05-05 22:13:12 -0700

    adding test controller file

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,8 +1,16 @@
 import pytest
-from models import DatabaseManager
+import mock
+import builtins
+from controllers import AppController
+
+
 
 @pytest.fixture
 def controller():
     return AppController()
 
 
+def test_running(controller):
+    with mock.patch.object(builtins, 'input', side_effect= [4]):
+        check = controller.run()
+        assert check == False

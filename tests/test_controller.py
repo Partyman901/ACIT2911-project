6441c09e315a549747efcfc43a38881247f42a91
Mo Mohseni <mohseni929@gmail.com>
2021-05-13 17:14:24 -0700

    new updated program with add function

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -28,6 +28,16 @@ def test_option_2(controller):
         assert check == False
         
 def test_option_2_valid(controller):
-    with mock.patch.object(builtins, 'input', side_effect=[2, "", "16291118 1812", 4]):
+    with mock.patch.object(builtins, 'input', side_effect=[2, "16291118 1812", 4, 4]):
+        check = controller.run()
+        assert check == False
+
+def test_option_3(controller):
+    with mock.patch.object(builtins, 'input', side_effect=[3, "mo","mohseni", "15151512 2020", 4]):
+        check = controller.run()
+        assert check == False
+
+def test_option_3_True(controller):
+    with mock.patch.object(builtins, 'input', side_effect=[3, "mo","mohseni", "15151512 2020","778452112", "specialist", 4]):
         check = controller.run()
         assert check == False
